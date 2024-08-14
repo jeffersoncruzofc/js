86b233a72db65dbef859a6d0418181c125b7410c
jeffersoncruzofc <jefferson.goncalves.cruz@escola.pr.gov.br>
2024-08-14 14:02:52 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 400);//cria uma página em branco
+}
+
+function draw() {
+  background('darkorange');//acresenta a cor do fundo 
+fill('green' )
+rect(10,10,80,130);//desenha um retangulo(posiçãox, posiçãoy, largura, altura)
+fill('white');
+rect(90,10,80,130);//CRIAR UM RETANGULO(POSIÇÃO X, POSIÇÃO Y, LARGURA,ALTURA)
+  
+fill('red')
+rect(170,10,80,130)
+
+
+
+}
